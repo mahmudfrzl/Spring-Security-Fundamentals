93181c1e54b9e8ecfbc761ca8c96f613436f74d9
Mahmud Ferzeli <mahmudfrzl@mail.ru>
2021-09-14 10:15:17 -0700

    Added ss-c2

---
+++ ss-c2/src/test/java/az/kalemhome/ssc2/SsC2ApplicationTests.java
@@ -0,0 +1,13 @@
+package az.kalemhome.ssc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsC2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
